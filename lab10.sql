a21cad2d798d4dd73710105b414145a8b117b8af
Artem1933 <167816082+Artem1933@users.noreply.github.com>
2024-11-27 01:28:08 +0200

    Add files via upload

---
+++ lab10.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: MySQL-8.2
+-- Время создания: Ноя 27 2024 г., 01:27
+-- Версия сервера: 8.2.0
+-- Версия PHP: 8.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `lab10`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lines`
+--
+
+CREATE TABLE `lines` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `color` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `stations`
+--
+
+CREATE TABLE `stations` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `line_id` int NOT NULL,
+  `coordinates` varchar(50) DEFAULT NULL,
+  `opened_date` date DEFAULT NULL,
+  `status` enum('active','under_construction','closed') DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `transfers`
+--
+
+CREATE TABLE `transfers` (
+  `id` int NOT NULL,
+  `station_from` int NOT NULL,
+  `station_to` int NOT NULL,
+  `distance` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `lines`
+--
+ALTER TABLE `lines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `stations`
+--
+ALTER TABLE `stations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `line_id` (`line_id`);
+
+--
+-- Индексы таблицы `transfers`
+--
+ALTER TABLE `transfers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `station_from` (`station_from`),
+  ADD KEY `station_to` (`station_to`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `lines`
+--
+ALTER TABLE `lines`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `stations`
+--
+ALTER TABLE `stations`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `transfers`
+--
+ALTER TABLE `transfers`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `stations`
+--
+ALTER TABLE `stations`
+  ADD CONSTRAINT `stations_ibfk_1` FOREIGN KEY (`line_id`) REFERENCES `lines` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `transfers`
+--
+ALTER TABLE `transfers`
+  ADD CONSTRAINT `transfers_ibfk_1` FOREIGN KEY (`station_from`) REFERENCES `stations` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `transfers_ibfk_2` FOREIGN KEY (`station_to`) REFERENCES `stations` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
